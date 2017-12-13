90d75fcc545985714519e9de0022abb3b362d19b
fuok <beast_7th@hotmail.com>
2017-12-13 17:38:20 +0800

    update code

--- Assets/AudioManagerS.cs
+++ Assets/AudioManagerS.cs
@@ -3,8 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 
 //音乐播放分为几种不同情况:
-//1,同一首音乐，则继续播放。2,同一首音乐，从头播放。
-//3,新的音乐，停止原先的播放新的。4,新的音乐，但同时播放。5,音效，无论相同的还是不同的，都同时播放
+//1,同一音源，继续播放。如背景音乐，切换场景时如果音乐相同，就没必要重新播放。
+//2,同一音源，重新播放。如格斗游戏的打击音效，一旦连击，即使上一次声音没播完，也马上结束播放新的。
+//3,不同音源，替代其他音源播放。同上，不同的打击效果音。
+//4,不同音源，和其他音源同时播放。最常见的音效。
+//情况1、2目前通过BGM方法可以实现，如果在其他地方使用要和情况3一起考虑。
 //但这个方法仍存在问题，比如人物语音，如何判断该停止播放哪段音源，除非把特定人物语音每次都挂载到特定AudioResource上。
 //那么这里能处理的，就只有背景音乐和特效了
 public class AudioManagerS : MonoBehaviour
@@ -16,7 +19,7 @@ public class AudioManagerS : MonoBehaviour
 	//音频剪辑索引
 	private Dictionary<string,AudioClip> mDicClip;
 
-	private string bgmName;
+	private string currentBGM;
 
 	void Awake ()
 	{
@@ -49,65 +52,50 @@ public class AudioManagerS : MonoBehaviour
 	}
 
 	/// <summary>
-	///传值 “音乐剪辑” 播放 “背景音乐”
+	/// Plaies the background.
 	/// </summary>
-	/// <param name="audiClip">游戏音效剪辑</param>
-	//public static void PlayEffAudio(AudioClip audiClip)
-	//{
-	//    if (audiClip)
-	//    {//播放
-	//        AudioSource[] AS = mAudioManagerInstance.GetComponents<AudioSource>();//先获取当前对象下的AudioSource
-	//        for (int i = 1; i < AS.Length; i++)
-	//        {//跳过下标0,0位预留给背景音乐
-	//            if (!AS[i].isPlaying)
-	//            {
-	//                AS[i].clip = audiClip;//音频剪辑的赋值
-	//                AS[i].Play();//播放背景音乐
-	//                return;
-	//            }
-	//            else
-	//            {
-	//                //元素被占用
-	//            }
-	//        }
-	//        //当需要同时播放多个音效或当前对象的AudioSource不足,添加新AudioSource
-	//        AudioSource NewAS = mAudioManagerInstance.AddComponent<AudioSource>();
-	//        NewAS.loop = false;
-	//        NewAS.clip = audiClip;
-	//        NewAS.Play();
-	//    }
-	//}
-
-	public void PlayBGM (string audioSourceName, float Volume)
+	/// <param name="audioSourceName">Audio source name.</param>
+	/// <param name="Volume">Volume.</param>
+	/// <param name="restartSame">If set to <c>true</c> restart same.</param>
+	public void PlayBGM (string audioSourceName, float Volume, bool restartSame = false)
 	{
-//		if (!string.IsNullOrEmpty (audioSourceName)) {
-//			if (mDicSource [audioSourceName]) {
-//				if (mDicSource [audioSourceName].isPlaying) {
-//					//如果当前bgm在播放，就什么也不做.
-//					return;
-//				}
-//				foreach (var item in transform.Find("bgm").GetComponentsInChildren<AudioSource>()) {
-//					//关闭其他bgm
-//					item.Stop ();
-//				}
-//				//AudioSource必须是组件形式才能播放，只是拿到对象（例如从字典里取出）是不行的。prefab的source也是不能播的,注意引用关系
-//				mDicSource [audioSourceName].Play ();
-//			}
-//		}
+		if (!string.IsNullOrEmpty (audioSourceName)) {
+			if (mDicClip [audioSourceName]) {
+				//AudioSource必须是组件形式才能播放，只是拿到对象（例如从字典里取出）是不行的。prefab的source也是不能播的,注意引用关系
+				AudioSource aSource = null;
+				foreach (var item in gameObject.GetComponents<AudioSource>()) {
+					if (currentBGM == currentBGM) {
+						aSource = item;
+						break;
+					}
+				}
+				aSource = aSource ?? GetLeisureAS ();
+
+				if (aSource.clip) {
+					if (aSource.clip.name == audioSourceName && aSource.isPlaying && !restartSame) {//判断是否正在播放、是否重新开始
+						//如果当前bgm在播放，就什么也不做.
+						print ("已经存在BGM，保持不变");
+						return;
+					} else {
+						print ("已经存在BGM，用当前AudioSource播放新BGM");
+					}
+
+				} else {
+					print ("不存在BGM，使用新AudioSource");
+				}
+
+				//关闭之前的bgm
+				aSource.Stop ();
+				//播放新的
+//				AudioSource aSource = GetLeisureAS ();
+				aSource.loop = true;
+				aSource.volume = Volume;
+				aSource.clip = mDicClip [audioSourceName];
+				aSource.Play ();
+				currentBGM = audioSourceName;
+			}
+		}
 	}
-
-	//PlayScheduled暂时还不太明白,TODO
-	//	public void PlayS (string audioClipName, double time)
-	//	{
-	//		if (!string.IsNullOrEmpty (audioClipName)) {
-	//			if (mDicClip [audioClipName]) {
-	//				AudioSource aSource = gameObject.AddComponent<AudioSource> ();
-	//				aSource.clip = mDicClip [audioClipName];
-	//				aSource.PlayScheduled (time);
-	//			}
-	//		}
-	//	}
-
 
 	/// <summary>
 	/// 可以同时播放相同Clip，适合播放音效,使用不断添加删除组件的方式
@@ -117,7 +105,7 @@ public class AudioManagerS : MonoBehaviour
 	{
 		if (!string.IsNullOrEmpty (audioClipName)) {
 			if (mDicClip [audioClipName]) {
-				AudioSource aSource = gameObject.AddComponent<AudioSource> ();
+				AudioSource aSource = GetLeisureAS ();
 				aSource.loop = false;
 				aSource.volume = Volume;
 				aSource.clip = mDicClip [audioClipName];
@@ -139,7 +127,7 @@ public class AudioManagerS : MonoBehaviour
 	{
 		if (!string.IsNullOrEmpty (audioClipName)) {
 			if (mDicClip [audioClipName]) {
-				AudioSource aSource = gameObject.AddComponent<AudioSource> ();
+				AudioSource aSource = GetLeisureAS ();
 				aSource.loop = true;
 				aSource.volume = Volume;
 				aSource.clip = mDicClip [audioClipName];
@@ -149,12 +137,12 @@ public class AudioManagerS : MonoBehaviour
 		}
 	}
 
-	//PlayOneShot搞得这么麻烦就和前面没什么区别了，如果要用这个还是直接传clip比较好
+	//PlayOneShot搞得这么麻烦就和前面没什么区别了，如果要用这个还是直接传clip比较好。PlayOneShot的特点是播放完后会把clip置空，但不会删除AudioSource
 	public void PlaySfxOneShot (string audioClipName, float Volume, bool autoDestroy = false)
 	{
 		if (!string.IsNullOrEmpty (audioClipName)) {
 			if (mDicClip [audioClipName]) {
-				AudioSource aSource = gameObject.AddComponent<AudioSource> ();
+				AudioSource aSource = GetLeisureAS ();
 				AudioClip clip = mDicClip [audioClipName];
 				if (clip) {
 					aSource.PlayOneShot (clip, Volume);
@@ -165,6 +153,18 @@ public class AudioManagerS : MonoBehaviour
 			}
 		}
 	}
+
+	//PlayScheduled暂时还不太明白,TODO
+	//	public void PlayS (string audioClipName, double time)
+	//	{
+	//		if (!string.IsNullOrEmpty (audioClipName)) {
+	//			if (mDicClip [audioClipName]) {
+	//				AudioSource aSource = gameObject.AddComponent<AudioSource> ();
+	//				aSource.clip = mDicClip [audioClipName];
+	//				aSource.PlayScheduled (time);
+	//			}
+	//		}
+	//	}
 
 	//----------------------------------------------------------------------------
 
@@ -190,5 +190,18 @@ public class AudioManagerS : MonoBehaviour
 		for (int i = 0; i < AS.Length; i++) {
 			AS [i].Stop ();
 		}
+	}
+
+	//----------------------------------------------------------------------------
+
+	//重复使用AudioSource组件
+	private AudioSource GetLeisureAS ()
+	{
+		foreach (var item in gameObject.GetComponents<AudioSource>()) {
+			if (!item.isPlaying) {
+				return item;
+			}
+		}
+		return gameObject.AddComponent<AudioSource> ();
 	}
 }
--- Assets/main.cs
+++ Assets/main.cs
@@ -4,10 +4,28 @@ using UnityEngine.UI;
 
 public class main : MonoBehaviour
 {
+	public Button btnSFX1, btnSFX2, btnSFX3;
+	public Button btnBGM1, btnBGM2;
 	public Button btnPlay, btnPause, btnStop;
 
 	void Start ()
 	{
+		btnSFX1.onClick.AddListener (() => {
+			AudioManagerS.Instance.PlaySFX ("ququSound", 1f);
+		});
+		btnSFX2.onClick.AddListener (() => {
+			AudioManagerS.Instance.PlaySFX ("thunder", 1f);
+		});
+		btnSFX3.onClick.AddListener (() => {
+			AudioManagerS.Instance.PlaySFX ("AudioEffect_C3_Fire(ShortGun)", 1f);
+		});
+
+		btnBGM1.onClick.AddListener (() => {
+			AudioManagerS.Instance.PlayBGM ("GameGUI_BackgroundAudio", 1f);
+		});
+		btnBGM2.onClick.AddListener (() => {
+			AudioManagerS.Instance.PlayBGM ("GamePlaying_BackgroundAudio", 1f);
+		});
 	
 		btnPlay.onClick.AddListener (() => {
 			AudioManagerS.Instance.PlayAll ();
@@ -24,14 +42,15 @@ public class main : MonoBehaviour
 	{
 	
 		if (Input.GetKeyDown (KeyCode.Alpha1)) {
-			AudioManagerS.Instance.PlaySFX ("thunder", 1f);
+			
 		}
+
 		if (Input.GetKeyDown (KeyCode.Alpha2)) {
-//			AudioManagerS.Instance.PlayOne ("ququSound", 1f);
+			
 		}
 
 		if (Input.GetKeyDown (KeyCode.Alpha3)) {
-			AudioManagerS.Instance.PlaySfxOneShot ("ququSound", 1f, true);
+
 		}
 
 	}
